be8c40f2a00e5af0fa058b4c5c3de9fd94beeeb9
rameshananth <rameshananth@users.noreply.github.com>
2017-07-14 17:06:42 -0400

    Update helper.js

--- convs/ServiceDesk/helper.js
+++ convs/ServiceDesk/helper.js
@@ -197,7 +197,7 @@ function makeWaterFall(dialogName,entryPoint,intentName){
 		function(session,args,next){
 			//logThis("Hi");
 			//logThis(session);
-			logThis(session.id+",Entering:"+dialogName+",TriggeredBy:"+intentName);
+			console.log(session.id+",Entering:"+dialogName+",TriggeredBy:"+intentName);
 			//Save the global intent to the conversation data
 			session.conversationData.intent=args.intent;
 			session.beginDialog(entryPoint.name,entryPoint.parameters);

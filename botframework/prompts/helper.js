6f7511d867c0bf871421ce5384be050fffaf9a7c
rameshananth <rameshananth@users.noreply.github.com>
2017-12-29 10:52:32 +0530

    Update helper.js

--- botframework/prompts/helper.js
+++ botframework/prompts/helper.js
@@ -114,8 +114,9 @@ lib.dialog('/CheckPrereqs',[
 			    console.log(args);
 			    session.dialogData.args=args;
 			    console.log(session.dialogData);
-			    session.beginDialog(args.check.name,args.check.parameters);
+			    
 		    }
+		    session.beginDialog(args.check.name,args.check.parameters);
 	    });
 	    
     }

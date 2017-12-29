3533d750e97514633088dc4e398d944e61197e47
rameshananth <rameshananth@users.noreply.github.com>
2017-12-29 12:27:06 +0530

    Update helper.js

--- botframework/prompts/helper.js
+++ botframework/prompts/helper.js
@@ -19,7 +19,7 @@ function logThis(results){
 lib.dialog('/Intent',[
 	function(session,args,next){
 		//console.log("Hello. You are in the intent function");
-		session.endDialogWithResult{success:true});
+		session.endDialogWithResult({success:true});
 	}
 ]);
 
